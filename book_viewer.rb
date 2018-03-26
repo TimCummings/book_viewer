e2e28cad64b5fbabefde7f6d1530871abab1f2ee
Tim Cummings <timjc86@gmail.com>
2018-03-26 00:39:11 -0500

    customize not_found route and redirect invalid chapter requests

--- book_viewer.rb
+++ book_viewer.rb
@@ -24,9 +24,16 @@ end
 
 get "/chapters/:number" do
   chapter_num = params[:number].to_i
+
+  redirect '/' unless (1..@toc.size).cover? chapter_num
+
   chapter_name = @toc[chapter_num - 1]
   @title = "Chapter #{chapter_num}: #{chapter_name}"
   @text = File.read "data/chp#{chapter_num}.txt"
 
   erb :chapter
+end
+
+not_found do
+  redirect '/'
 end
