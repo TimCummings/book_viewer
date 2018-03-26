0d5710d1ea1e0ea21f23953fa00d308cae7da89f
Tim Cummings <timjc86@gmail.com>
2018-03-25 23:48:19 -0500

    improve route parameters; add detail to chapter titles and links

--- book_viewer.rb
+++ book_viewer.rb
@@ -10,10 +10,13 @@ get "/" do
   erb :home
 end
 
-get "/chapters/:number" do |n|
-  @title = "Chapter #{n}"
+get "/chapters/:number" do
   @toc = File.readlines "data/toc.txt"
-  @text = File.read "data/chp#{n}.txt"
 
+  chapter_num = params[:number].to_i
+  chapter_name = @toc[chapter_num - 1]
+  @title = "Chapter #{chapter_num}: #{chapter_name}"
+
+  @text = File.read "data/chp#{chapter_num}.txt"
   erb :chapter
 end
